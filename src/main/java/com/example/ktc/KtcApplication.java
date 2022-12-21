d5dece7b7cb540ab876a18ddf57fd6cf7bce8e78
hjs9605 <hjs9605@naver.com>
2022-12-21 16:03:00 +0900

    first

---
+++ src/main/java/com/example/ktc/KtcApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtcApplication.class, args);
+	}
+
+}
